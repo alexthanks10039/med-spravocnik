8a8dff43ae2bd21422d5531ff1e18b1d072c4826
alexthanks10039 <alexthanks10039@gmail.com>
2026-09-19 07:24:34 +0500

    refactor: add graceful server shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-import { app } from './app.js'; import { env } from './config/env.js'; app.listen(env.PORT,()=>console.log(`MED SPRAVOCHNIK: http://localhost:${env.PORT}`));
+import { app } from './app.js';
+import { env } from './config/env.js';
+import { prisma } from './shared/prisma.js';
+
+const server = app.listen(env.PORT, () => {
+  console.log('MED SPRAVOCHNIK: http://localhost:' + env.PORT);
+});
+
+const shutdown = (signal: string) => {
+  console.log('Received ' + signal + ', shutting down...');
+
+  server.close(() => {
+    void prisma.$disconnect().finally(() => process.exit(0));
+  });
+};
+
+process.once('SIGINT', () => shutdown('SIGINT'));
+process.once('SIGTERM', () => shutdown('SIGTERM'));
